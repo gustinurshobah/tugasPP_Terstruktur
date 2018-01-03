a4b546121abba64037f7ad2df13adbe9000519a1
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2018-01-03 23:18:22 +0800

    mendeklarasikan varibel

--- SAW.cpp
+++ SAW.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 int main(){
 	// DEKLARASI VARIABEL
-	double a1c1,a1c2,a1c3,a1c4,a1c5;
-	double a2c1,a2c2,a2c3,a2c4,a2c5;
 	double a[3][5]={{4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2}};
 		
 	double w[5]={5,3,4,4,2};
